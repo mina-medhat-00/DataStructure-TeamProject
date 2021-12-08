5a04b3f31d480eb247fb26fd1220fcd81c2ee420
HelalyJunior <1808447@eng.asu.edu.eg>
2021-12-08 14:27:10 +0200

    adding files

---
+++ VisualStudioProjectFiles/XML/XML/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace XML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string s1 = "Amr";
+            string s2 = s1;
+            Console.WriteLine(s2, s1);
+        }
+    }
+}
